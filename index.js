46309b0d6c241b5bf284b83e7513f20cf010ab23
fadrizul <fadrizul@gmail.com>
2014-04-11 17:02:33 +0800

    (IMPROVE) Implement semi-colons & function scoping

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ Copyright (c) MyTeksi GrabTaxi 2013
 Author: Fadrizul Hasani <fadrizul@gmail.com>
 */
 
-var redisManager = require('redis-client-manager')
+(function () {
+  var redisManager;
 
-exports.push = function (connectionOptions, workerName, workerArguments, retry, callback) {
-  var connectionOptions = connectionOptions || {}
-  var message = {
-    'class': workerName,
-    'args': workerArguments,
-    'retry': retry
-  }
-  redisManager.getClient(connectionOptions).lpush('frontend:queue:default', JSON.stringify(message), callback)
-}
+  redisManager = require('redis-client-manager');
+
+  exports.push = function (connectionOptions, workerName, workerArguments, retry, callback) {
+    var connectionOptions, message;
+
+    connectionOptions = connectionOptions || {};
+    message = {
+      'class': workerName,
+      'args': workerArguments,
+      'retry': retry
+    };
+
+    redisManager.getClient(connectionOptions).lpush('frontend:queue:default', JSON.stringify(message), callback);
+  };
+}).call(this)
